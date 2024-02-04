9ff80b14af8169695e34b09873c232b752f6c055
Marco Acea <aceamarco@gmail.com>
2024-02-03 21:33:28 -0500

    Update index.js

--- server/src/index.js
+++ server/src/index.js
@@ -1,1 +1,18 @@
-console.log("Hello World");
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+app.use(express.static("dist"));
+app.use(cors());
+
+
+app.get("/", (req, res) => {
+  res.send("Hello, World!");
+});
+
+app.get("/")
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
