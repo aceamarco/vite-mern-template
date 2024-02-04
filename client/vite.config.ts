341a766493a23c700c2cdffe13639f9c72df65cb
Marco Acea <aceamarco@gmail.com>
2024-02-03 21:04:07 -0500

    Update vite.config.ts

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,21 +1,16 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import svgrPlugin from "vite-plugin-svgr";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  plugins: [react()],
+
   server: {
-    port: 3000,
     proxy: {
-      "/api": "Enter your backend host with port",
+      "/api": {
+        target: "http://localhost:3001",
+        changeOrigin: true,
+      },
     },
   },
-  plugins: [
-    react(),
-    svgrPlugin({
-      svgrOptions: {
-        icon: true,
-      },
-    }),
-  ],
 });
